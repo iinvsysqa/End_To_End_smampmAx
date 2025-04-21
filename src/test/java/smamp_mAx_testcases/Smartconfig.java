e13b8ad0c7aabac78e74e4d028f530cb03044ebc
unknown <pkirupakaran@gmail.com>
2025-04-21 13:18:36 +0530

    Update Schedule

--- src/test/java/smamp_mAx_testcases/Smartconfig.java
+++ src/test/java/smamp_mAx_testcases/Smartconfig.java
@@ -69,7 +69,7 @@ public class Smartconfig extends MobileAppWrappers {
 		logReadandWrite readwrite = logReadandWrite.getInstance(loadProp("COM"));
 		try {
 			readwrite.openPort();
-			adddevicepage.pair(3);
+			/*adddevicepage.pair(3);
 			adddevicepage.blepermissionokpopup();
 			//sZephyr info page check
 			sZephyrinfopage.deviceNameCheck(loadProp("USERNAMEINAPP"));
@@ -87,7 +87,10 @@ public class Smartconfig extends MobileAppWrappers {
 			adddevicepage.checkdevicesettingstoast();
 			
 			//home page check
-			devicemenupage.checkUsername_devicesettings("Home page");
+			devicemenupage.checkUsername_devicesettings("Home page");*/
+			schedularpage.clickSchedulebtn();
+			schedularpage.createSchedules(3, 1, 1);
+			
 			adddevicepage.bleConnectivityCheck();
 			homepage.getCurrentvalue();
 			homepage.getVoltvalue();
